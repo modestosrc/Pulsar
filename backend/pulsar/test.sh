cad97fe5b3bd40b996ac80515435bb6f7af3b345
Mateus <ext.mateus@icloud.com>
2025-04-29 12:21:02 -0300

    Esse e um commit teste

--- backend/pulsar/test.sh
+++ backend/pulsar/test.sh
@@ -5,3 +5,5 @@ curl localhost:8080/card/create?col=0 -H "Content-Type: text/plain" -d "Teste 3"
 
 echo "Teste /card/get"
 curl localhost:8080/card/get?col=0 -H "Content-Type: text/plain"
+
+# Commit teste
